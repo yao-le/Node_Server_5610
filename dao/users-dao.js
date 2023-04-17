ce10f574b2e25b6a914af579e442f0272a564741
yao-le <le.yao@northeastern.edu>
2023-04-17 14:40:01 -0400

    sort users by createdAt

--- dao/users-dao.js
+++ dao/users-dao.js
@@ -3,5 +3,5 @@ import {usersModel} from "../models/users-model.js";
 
 export const findUserById = (uid) => usersModel.findById(uid);
 export const findUserByCredentials = (name, password) => usersModel.findOne({name, password});
-export const findAllUsers = () => usersModel.find();
+export const findAllUsers = () => usersModel.find().sort({createdAt: -1});
 export const deleteUser= (uid) => usersModel.deleteOne({_id: uid});
