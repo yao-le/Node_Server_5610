4b81c19815a1a743d97aae58f546749744d18b00
yao-le <le.yao@northeastern.edu>
2023-04-17 13:44:58 -0400

    change base user schema

--- schemas/baseUsers-schema.js
+++ schemas/baseUsers-schema.js
@@ -9,7 +9,7 @@ const baseUsersSchema = new mongoose.Schema({
     name: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     email: String,
-    portrait: {type: String, default: "https://cdn2.iconfinder.com/data/icons/communication-489/24/account_profile_user_contact_person_avatar_placeholder-512.png"},
+    portrait: String,
     createdAt: { type: Date, default: Date.now },
 }, options);
 
