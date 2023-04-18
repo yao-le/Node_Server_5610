e51014a630f1736bbed07afc0534e3a821eccce1
yao-le <le.yao@northeastern.edu>
2023-04-18 10:59:05 -0400

    modify albums schema

--- schemas/albums-schema.js
+++ schemas/albums-schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const albumsSchema = new mongoose.Schema({
     albumName: String,
     publisher: {type: mongoose.Schema.Types.ObjectId, ref: "publishers"},
-    publisherName: String,
+    // publisherName: String,
     coverPic: String,
     description: String,
     createdAt: {type: Date, default: Date.now},
